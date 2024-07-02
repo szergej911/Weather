3e53b1f389d47e72e5dc7ce98a5b40f1941e7d1f
Németh Gergely <szergej911@gmail.com>
2024-07-02 21:38:59 +0200

    Adding Tailwind and WeatherList component

--- src/components/WeatherList.tsx
+++ src/components/WeatherList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { WeatherData } from "../types/api";
 
 interface WeatherListProps {
@@ -6,19 +6,33 @@ interface WeatherListProps {
 }
 
 const WeatherList: React.FC<WeatherListProps> = ({ weatherHistory }) => {
+  const [clickedLocation, setClickedLocation] = useState<string | null>(null);
+
+  const handleClick = (id: number) => {
+    setClickedLocation(weatherHistory[id].location.name);
+  };
+
   return (
     <div>
       <h1>Last 4 Searches</h1>
       {weatherHistory.length > 0 ? (
-        weatherHistory.map((weather, index) => (
-          <div key={index}>
+        weatherHistory.slice(-4).map((weather, index) => (
+          <div
+            onClick={() => handleClick(index)}
+            key={index}
+            style={{ cursor: "pointer", margin: "10px 0" }}
+          >
             <h2>Weather in {weather.location.name}</h2>
             <p>Temperature: {weather.current.temp_c}°C</p>
-            <p>Description: {weather.current.condition.text}</p>
           </div>
         ))
       ) : (
         <p>No weather data stored.</p>
+      )}
+      {clickedLocation && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>Clicked Location: {clickedLocation}</h2>
+        </div>
       )}
     </div>
   );
